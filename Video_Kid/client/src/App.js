e7c4adde5caa24c126ed7e23de356554af25113d
Vu-Trung-Nam <vutrungnam2002@gmail.com>
2022-12-08 08:24:38 +0700

    update cac file

--- Video_Kid/client/src/App.js
+++ Video_Kid/client/src/App.js
@@ -7,14 +7,33 @@ import Search from './pages/Search';
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import { useSelector } from "react-redux";
-import Channels from "./pages/Channels";
 import { useState } from "react";
-import { Theme1, Theme2 } from "./redux/theme/Theme";
 
+const Container = styled.div`
+  display: flex;
+  
+`;
+
+const Main = styled.div`
+  flex: 7;
+`;
+
+
+const Wrapper=styled.div`
+flex: 7;
+background-color: #cfcede;
+min-height: 100vh;
+;`
 
 function App() {
   return (
-   <div>helo</div>
+   <Container>
+      <Menu></Menu>
+      <Main>
+          <Navbar></Navbar>
+          <Wrapper></Wrapper>
+      </Main>
+   </Container>
   );
 }
 
