eaf7fcd73958dd8545e9bdba03482a9a24f391d1
Vu-Trung-Nam <vutrungnam2002@gmail.com>
2022-12-08 09:09:16 +0700

    update navbar

--- Video_Kid/client/src/components/Navbar.jsx
+++ Video_Kid/client/src/components/Navbar.jsx
@@ -142,7 +142,7 @@ const Navbar = () => {
           )}
             {currentUser && <>
               <Button onClick={logout}>
-              Đăng xuất
+              ĐĂNG XUẤT
             </Button>
             </>
             }
