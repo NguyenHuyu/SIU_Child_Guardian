72f2b8f27083058ebeb54aa03e4aee68c4bc1c95
Vu-Trung-Nam <vutrungnam2002@gmail.com>
2022-12-08 09:10:20 +0700

    update page home

--- Video_Kid/client/src/pages/Home.jsx
+++ Video_Kid/client/src/pages/Home.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import Card from '../components/Card';
+import axios from "axios";
 
-const Home = () => {
+
+const Container=styled.div`
+display: flex;
+flex-wrap: wrap;
+margin-top: -5px;
+`
+const Home = ({type}) => {
+  
+  const [Videos,setVideos]=useState([]);
+
+  useEffect(()=>{
+      const fetchVideos=async() =>{
+        const res=await axios.get(`/videos/${type}`);
+        setVideos(res.data);
+      };
+      fetchVideos();
+    },[type]);
+
+  
+  
+  
+  
   return (
     <div>
-      home
+    <Container>
+      {Videos.map((video)=>(<Card key={video._id} video={video} ></Card> ) )}
+   
+    </Container>
     </div>
   )
 }
