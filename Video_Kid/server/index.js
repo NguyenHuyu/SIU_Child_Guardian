687e0a488fc6b73764fb95dfa862fe005ef4d671
Nguyễn Minh Huy <yuhuynguyen.1204@gmail.com>
2022-12-08 09:17:57 +0700

    :sparkles: Get User

--- Video_Kid/server/index.js
+++ Video_Kid/server/index.js
@@ -1,8 +1,9 @@
 import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
-import authRoutes from "./routes/auths.js"
 import cookieParser from "cookie-parser"
+import authRoutes from "./routes/auths.js"
+import userRoutes from "./routes/users.js"
 
 const app = express()
 const PORT = 10000
@@ -13,6 +14,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
 
 
 mongoose.connect(process.env.URI_key,{
