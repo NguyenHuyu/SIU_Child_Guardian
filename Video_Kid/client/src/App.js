30f437e7f48a937279ec3b585650bf36fbfbb081
Vu-Trung-Nam <vutrungnam2002@gmail.com>
2022-12-08 08:20:30 +0700

    update app.js

--- Video_Kid/client/src/App.js
+++ Video_Kid/client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { ThemeProvider } from "styled-components";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar"
+import Home from './pages/Home';
+import Video from './pages/Video';
+import Search from './pages/Search';
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import { useSelector } from "react-redux";
+import Channels from "./pages/Channels";
+import { useState } from "react";
+import { Theme1, Theme2 } from "./redux/theme/Theme";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>helo</div>
   );
 }
 
