55b227abd530417363fdf63e9e7b3d1087778c30
Nguyễn Minh Huy <yuhuynguyen.1204@gmail.com>
2022-12-08 10:49:43 +0700

    :sparkles: Random Video

--- Video_Kid/server/routes/videos.js
+++ Video_Kid/server/routes/videos.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { addVideo, deleteVideo, getVideo } from "../controllers/video.js"
+import { addVideo, deleteVideo, getVideo, random } from "../controllers/video.js"
 
 const  router = express.Router()
 
 router.get("/find/:id", getVideo)
 router.post("/", addVideo)
 router.delete("/:id", deleteVideo)
+router.get("/random", random)
 
-export default router
+export default router   
