42c701d72bae45af1e8c8a4df8ba58280ec31351
Nguyễn Minh Huy <yuhuynguyen.1204@gmail.com>
2022-12-08 09:06:58 +0700

    :sparkles: SignIn

--- Video_Kid/server/controllers/auth.js
+++ Video_Kid/server/controllers/auth.js
@@ -18,3 +18,20 @@ export const register = async(req, res, next)=>{
     }
 }
 
+export const login = async(req, res, next)=>{
+    try {
+        const user = await User.findOne({name: req.body.name})
+        if(!user)
+            return res.status(403).send("Không tìm thầy tài khoản")
+        const isCorrect = await bcrypt.compare(req.body.password, user.password)
+        if(!isCorrect)
+            return res.status(403).send("Tài khoản không trùng khớp")
+        const token = jwt.sign({id:user._id}, process.env.Access_key)
+        const {password, ...others} = user._doc
+        res.cookie("access_token", token, {
+            httpOnly:true   
+        }).status(200).json(others)
+    } catch (error) {
+        next(error)
+    }
+}
--- Video_Kid/server/routes/auths.js
+++ Video_Kid/server/routes/auths.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { register } from "../controllers/auth.js"
+import { login, register } from "../controllers/auth.js"
 
 const router = express.Router()
 
 
 router.post("/signup", register)
+router.post("/signin", login)
 
 export default router
